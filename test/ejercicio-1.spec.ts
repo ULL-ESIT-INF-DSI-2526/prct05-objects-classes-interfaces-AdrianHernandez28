bce7c70e637c3c9d16cabca0feee2bd4fd6ddbf2
Adrian Hernandez <alu0101637594@ull.edu.es>
2026-02-26 11:20:24 +0000

    Prueba de funcionamiento de CI github workflows

--- test/ejercicio-1.spec.ts
+++ test/ejercicio-1.spec.ts
@@ -1,2 +1,11 @@
 import { describe, expect, test } from 'vitest';
+import { add } from "./src/ejercicio-1"
+
+describe("", () => {
+    test("", () => {
+        expect(add(1, 2)).toEqual(3);
+    })
+});
+
+
 
